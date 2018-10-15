16da06d6775c046e5bb9bb45406764fe87000d04
Phidica Veia <phidica.veia@gmail.com>
2018-10-16 00:33:41 +1030

    Restructure do_fish_indent to parse only the current selection
    This forms the basis of a process towards parsing multiple selections, and
    parsing the entire file if no selections exist
    
    Note that this immediately renders all of the docs out of date

--- do_fish_indent.py
+++ do_fish_indent.py
@@ -32,75 +32,90 @@ class DoFishIndentCommand(sublime_plugin.TextCommand):
     if pathToDir:
       exe = os.path.join(pathToDir, exe)
 
-    # Select the entire contents of the file
-    fileRegion = sublime.Region(0, self.view.size())
-    fileContent = self.view.substr(fileRegion)
+    # Note the user's current settings for this buffer
+    indentUsingSpaces = self.view.settings().get('translate_tabs_to_spaces')
+    tabSize = self.view.settings().get('tab_size')
+
+    # If user does not use the native fish_indent format, convert file to tabs
+    #   of user's preferred width. Then when the formatted text is inserted,
+    #   we can convert it to tabs of width 4 without affecting anything else in
+    #   the file
+    # Note that unexpand_tabs sets translate_tabs_to_spaces to false
+    if not indentUsingSpaces or not tabSize == 4:
+      self.view.run_command('unexpand_tabs')
 
     # Note the file encoding, converting to lowercase as expected by Python
-    # However, fish_indent assumes UTF-8 encoding so the user may get unexpected results if this file's encoding is different
+    # However, fish_indent assumes UTF-8 encoding so the user may get
+    #   unexpected results if this file's encoding is different
     enc = self.view.encoding().lower()
     if enc == 'undefined': # ie, temp file
       enc = 'utf-8'
     print('Running {0} on file with encoding {1}'.format(exe, enc))
 
+    # Create a copy of all current cursor positions (selection regions)
+    # pos = list( self.view.sel() );
+
+    # Select the first region
+    # inputRegion = sublime.Region(0, self.view.size())
+    inputRegion = self.view.sel()[0];
+    inputContent = self.view.substr(inputRegion)
+
     # Run the program, which is searched for on PATH if necessary
     try:
       # Pipe the file content into fish_indent and catch the outputs
-      p = subprocess.Popen(exe, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-      out, err = p.communicate(input = fileContent.encode(enc))
+      p = subprocess.Popen(exe, stdin = subprocess.PIPE, stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE)
+      out, err = p.communicate(input = inputContent.encode(enc))
     except OSError: # Soft compatibility with Python 2
       msg = "Couldn't find {0}".format(exe)
       if not pathToDir:
-        msg += ". Specify a nonstandard install location in Preferences > Package Settings > friendly interactive shell > Settings"
+        msg += ". Specify a nonstandard install location in Preferences > " \
+          "Package Settings > friendly interactive shell > Settings"
       sublime.error_message(msg)
       return
 
     if err:
       sublime.message_dialog(err.decode('utf-8'))
 
-    # Create a copy of all current cursor positions
-    pos = list( self.view.sel() );
+    # Replace the contents of the region with the output of fish_indent
+    # We don't use replace() because we need to recalculate the region size
+    #   to adjust the cursor position, since the formatted text will almost
+    #   always have more characters than before
+    self.view.erase(edit, inputRegion)
+    insChars = self.view.insert(edit, inputRegion.begin(), out.decode(enc))
+    outputRegion = sublime.Region(inputRegion.begin(),
+      inputRegion.begin() + insChars)
 
-    # Replace the entire contents of the file with the output of fish_indent
-    self.view.replace(edit, fileRegion, out.decode(enc))
-
-    # Note the user's current settings for this buffer
-    indentUsingSpaces = self.view.settings().get('translate_tabs_to_spaces')
-    tabSize = self.view.settings().get('tab_size')
+    # Add new selection region, which encompasses original region and replaces it
+    self.view.sel().add(outputRegion)
 
     # Convert the format to the user's preferred format
     if indentUsingSpaces and tabSize == 4:
       # Do nothing as this is the format produced by fish_indent
       pass
     else:
-      # Convert sets of 4 spaces to tabs
-      # Note that running unexpand_tabs will set translate_tabs_to_spaces to False
+      # Convert sets of 4 spaces to tabs in all the fish_indent output
       self.view.settings().set('tab_size', 4)
       self.view.run_command('unexpand_tabs')
 
       if not indentUsingSpaces:
-        # User prefers tabs
+        # User prefers tabs, which we now have
         if tabSize == 4:
           # Conversion finished
           pass
         else:
-          # Resize
+          # Resize everything back to the preferred tab size
           self.view.settings().set('tab_size', tabSize)
       else:
-        # User prefers spaces, so reset to True
+        # User prefers spaces, so set back to true after unexpand_tabs changed it
         self.view.settings().set('translate_tabs_to_spaces', True)
 
         # Resize tabs, then convert back into spaces
         self.view.settings().set('tab_size', tabSize)
         self.view.run_command('expand_tabs')
 
-    # Revert back to the old cursor positions and centre on the first one
-    self.view.sel().clear()
-    if versionAPI == 3:
-      self.view.sel().add_all(pos)
-    elif versionAPI == 2:
-      map(self.view.sel().add, pos)
-    self.view.show_at_center(pos[0])
+    # Ensure cursors remain in view and don't drift off with repeated runs
+    # self.view.show(self.view.sel(), True)
 
 # Only a WindowCommand can be a build system target
 class DoFishIndentBuildCommand(sublime_plugin.WindowCommand):
@@ -110,7 +125,8 @@ class DoFishIndentBuildCommand(sublime_plugin.WindowCommand):
 if sublime.version()[0] == '3':
   class DoFishIndentOnSave(sublime_plugin.ViewEventListener):
     def is_applicable(self):
-      # In non-fish files the setting is unset (hence, we ask for False), or if the user set it to False then it will be False even in fish files
+      # In non-fish files the setting is unset (hence, we ask for False), or if
+      #   the user set it to False then it will be False even in fish files
       return self.get('indent_on_save', False)
 
     def on_pre_save(self):
